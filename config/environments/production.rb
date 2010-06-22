d76fc662b399da35fc8ebc26a98f619b17d27849
Daniel X Moore <yahivin@gmail.com>
2010-06-21 20:10:16 -0700

    Added more mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ PixieStrd6Com::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "localhost",
     :port => "25",
