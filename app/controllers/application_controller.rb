9e6013cd70eaca01ec9ba1f2350d98991ab4d46e
Daniel X Moore <yahivin@gmail.com>
2010-07-08 11:51:28 -0700

    Revert "Pretty busted beginning of adding twitter auth, shoving it into a branch giving up"
    This reverts commit 462e165b1093ecd10302884f7fd601f607bdc4d7.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  helper :all
   protect_from_forgery
   layout 'application'
 
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,13 +6,11 @@ class UserSessionsController < ApplicationController
   def create
     # Default to remember me
     @user_session = UserSession.new(params[:user_session].merge(:remember_me => true))
-    @user_session.save do |result|
-      if result
-        flash[:notice] = "Login successful!"
-        redirect_back_or_default root_url
-      else
-        render :action => :new
-      end
+    if @user_session.save
+      flash[:notice] = "Successfully logged in."
+      redirect_to root_url
+    else
+      render :action => 'new'
     end
   end
 
