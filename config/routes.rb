181f3a5987fc1f4321229e3dd05301ee010eb554
Daniel X Moore <yahivin@gmail.com>
2010-07-12 10:59:05 -0700

    Fixed upload path from within namespace and reorganized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 PixieStrd6Com::Application.routes.draw do |map|
+  namespace :abingo do
+    match "dashboard" => 'dashboard#index'
+  end
+
   resources :feedbacks do
     collection do
       get :thanks
     end
   end
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   resources :sprites do
     member do
@@ -34,39 +24,6 @@ PixieStrd6Com::Application.routes.draw do |map|
 
   resources :favorites
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
   # Catch old urls
   match 'creation(/:dummy(/:dummy))' => "sprites#new"
 
@@ -82,11 +39,33 @@ PixieStrd6Com::Application.routes.draw do |map|
 
   match 'users/remove_favorite/:id' => 'users#remove_favorite'
 
-  namespace :abingo do
-    match "dashboard" => 'dashboard#index'
-  end
-
   root :to => "home#index"
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get :recent, :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
 
   # See how all your routes lay out with "rake routes"
 
