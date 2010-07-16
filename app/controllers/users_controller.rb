e521f395c6ceba171a68c66c6f991e6cf2536e32
Daniel X Moore <yahivin@gmail.com>
2010-07-16 01:15:58 -0700

    Sleepyness bugs fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,18 +78,6 @@ class UsersController < ResourceController::Base
     end
   end
 
-  def load_user_using_perishable_token
-    @user = User.find_using_perishable_token(params[:id])
-
-    unless @user
-      flash[:notice] = "We're sorry, but we could not locate your account. " +
-        "If you are having issues try copying and pasting the URL " +
-        "from your email into your browser or restarting the " +
-        "reset password process."
-      redirect_to root_url
-    end
-  end
-
   def object
     return @object if defined?(@object)
 
