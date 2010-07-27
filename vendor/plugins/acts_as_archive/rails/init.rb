4f29c9db5b96ba6a4865646e4f0550ea7a04fe46
Daniel X Moore <yahivin@gmail.com>
2010-07-26 20:29:52 -0700

    Removed ol acts_as_archive

--- vendor/plugins/acts_as_archive/rails/init.rb
+++
@@ -1,5 +0,0 @@
-require File.expand_path("#{File.dirname(__FILE__)}/../require")
-Require.rails_init!
-
-ActiveRecord::Base.send(:include, ActsAsArchive::Base)
-ActiveRecord::Migration.send(:include, ActsAsArchive::Migration)
