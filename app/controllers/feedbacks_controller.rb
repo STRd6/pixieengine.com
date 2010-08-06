db6c9892e59d759db07f3a0d050de12d66e26cb2
Daniel X Moore <yahivin@gmail.com>
2010-08-06 00:04:59 -0700

    Added logged in user reference to Feedback when created

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,16 @@
 class FeedbacksController < ResourceController::Base
   actions :all, :except => [:index, :show, :edit, :update, :destroy]
 
+  create.before do
+    feedback.user = current_user
+  end
+
   create.response do |wants|
     wants.html { redirect_to :action => :thanks }
+  end
+
+  private
+  def feedback
+    object
   end
 end
