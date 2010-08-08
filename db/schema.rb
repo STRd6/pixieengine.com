06cc31d833316c0c87c0da2fe49a02434a6a8034
Jeff Zych <jlzych@gmail.com>
2010-08-07 17:34:56 -0700

    Eliminated Blueprint and moved all relevant styles into main.sass. Split form styles into their own sass file.

--- db/schema.rb
+++ db/schema.rb
@@ -69,9 +69,9 @@ ActiveRecord::Schema.define(:version => 20100804222146) do
     t.integer  "user_id"
     t.string   "title"
     t.text     "description"
-    t.integer  "parent_id"
-    t.integer  "comments_count"
     t.datetime "deleted_at"
+    t.integer  "parent_id"
+    t.integer  "comments_count",     :default => 0,     :null => false
     t.boolean  "replayable",         :default => false, :null => false
     t.string   "image_file_name"
     t.string   "image_content_type"
