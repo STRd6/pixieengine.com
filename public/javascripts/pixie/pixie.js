cabd36a8c7484f20de7958ad0abfd669e199feb0
Daniel X Moore <yahivin@gmail.com>
2010-08-08 11:23:18 -0700

    Added orange and ivory to default colors

--- public/javascripts/pixie/pixie.js
+++ public/javascripts/pixie/pixie.js
@@ -960,7 +960,7 @@
         canvas.addAction(action);
       });
 
-      $.each(["#000", "#FFF", "#666", "#DCDCDC", "#EB070E", "#388326", "#0246E3", "#FFDE49", "#563495", "#58C4F5", "#E5AC99", "#5B4635"], function(i, color) {
+      $.each(["#000", "#FFF", "#666", "#DCDCDC", "#EB070E", "#F69508", "#FFDE49", "#388326", "#0246E3", "#563495", "#58C4F5", "#E5AC99", "#5B4635", "#FFFEE9"], function(i, color) {
         canvas.addSwatch(color);
       });
 
