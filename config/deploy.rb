3918a52302539585a3ab03a549f97ec722f8146e
Daniel X Moore <yahivin@gmail.com>
2010-08-12 23:11:17 -0700

    Removed database config from git

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,6 +45,7 @@ task :after_update_code do
   run "ln -nfs #{shared_path}/local/authlogic.yml #{release_path}/config/authlogic.yml"
   run "ln -nfs #{shared_path}/local/local.rake #{release_path}/lib/tasks/local.rake"
   run "ln -nfs #{shared_path}/local/s3.yml #{release_path}/config/s3.yml"
+  run "ln -nfs #{shared_path}/local/s3.yml #{release_path}/config/database.yml"
 end
 
 # Passenger start Tasks
