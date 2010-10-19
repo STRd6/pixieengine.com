a1e4d674ebd6d66cadf74cc2908f664597a8c4ac
Daniel X Moore <yahivin@gmail.com>
2010-10-18 18:53:43 -0700

    Added ability to change new sprite size

--- app/models/app_sprite.rb
+++ app/models/app_sprite.rb
@@ -2,15 +2,6 @@ class AppSprite < ActiveRecord::Base
   belongs_to :app
   belongs_to :sprite
 
-  def data_url
-    sprite.data_url
-  end
-
-  def title
-    sprite.title
-  end
-
-  def user
-    app.user
-  end
+  delegate :data_url, :width, :height, :to => :sprite
+  delegate :user, :to => :app
 end
