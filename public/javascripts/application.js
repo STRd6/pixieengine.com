a49a1858914c8bf39ccb56474d41887704936279
Daniel X Moore <yahivin@gmail.com>
2010-11-05 17:38:39 -0700

    Added tips to IDE

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,12 +11,12 @@ function notify(message) {
 function showTooltip(element, html) {
   var position = element.offset();
 
-  position.left += element.width() + 16;
-  $("#tooltip").offset(position).fadeIn().find(".content").html(html);
+  position.left += element.width() + 42;
+  $("#tooltip").stop().offset(position).fadeIn().find(".content").html(html);
 }
 
 function hideTooltip() {
-  $("#tooltip").fadeOut();
+  $("#tooltip").stop().fadeOut();
 }
 
 function trackPageview(pageName) {
