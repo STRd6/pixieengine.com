e1549a3c42412aa63b87b3caed6084e4f17e81fc
Daniel X Moore <yahivin@gmail.com>
2011-01-12 00:04:06 -0800

    Added test and dev s3 aliases

--- config/initializers/s3_opts.rb
+++ config/initializers/s3_opts.rb
@@ -1,11 +1,13 @@
 S3_OPTS = {
   :storage => :s3,
   :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+  :url => ':s3_alias_url'
 }
 
-if Rails.env.production?
-  S3_OPTS.merge!(
-    :s3_host_alias => "images.pixie.strd6.com",
-    :url => ':s3_alias_url'
-  )
+if Rails.env.test?
+  S3_OPTS.merge!(:s3_host_alias => "test.pixie.strd6.com")
+elsif Rails.env.development?
+  S3_OPTS.merge!(:s3_host_alias => "dev.pixie.strd6.com")
+elsif Rails.env.production?
+  S3_OPTS.merge!(:s3_host_alias => "images.pixie.strd6.com")
 end
