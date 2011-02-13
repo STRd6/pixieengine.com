0184ff8e6e58c158a2040b9d0ab0243c8e92ab4c
Daniel X Moore <yahivin@gmail.com>
2011-02-13 15:28:15 -0800

    Reports, round1, Fight\!

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,4 +4,8 @@ class Visit < ActiveRecord::Base
   def self.track(user, controller, action)
     Visit.create(:user => user, :controller => controller, :action => action)
   end
+
+  def self.daily_active_report
+    Visit.select("COUNT(DISTINCT user_id) as count, date_trunc('day', created_at) AS day").group("date_trunc('day', created_at)").order("day")
+  end
 end
