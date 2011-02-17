cae6bd678a6d5d6275defd666535ba4ac198b041
Daniel X Moore <yahivin@gmail.com>
2011-02-16 16:12:35 -0800

    Added js caching and site coffee

---
+++ public/javascripts/site.js
@@ -0,0 +1,7 @@
+/* DO NOT MODIFY. This file was compiled Wed, 16 Feb 2011 23:52:34 GMT from
+ * /home/daniel/apps/pixie.strd6.com/app/coffeescripts/site.coffee
+ */
+
+(function() {
+
+}).call(this);
