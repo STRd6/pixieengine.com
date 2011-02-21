05b5b0dfc308d1910b38459d1c89476b4df84d5c
Matt Diebolt <mdiebolt@gmail.com>
2011-02-21 15:31:59 -0800

    made the sfx editor fullscreen

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,11 +1,20 @@
 class SoundsController < ResourceController::Base
   actions :all
+  respond_to :html, :json
 
   before_filter :require_owner_or_admin, :only => [:destroy, :edit, :update]
   before_filter :require_user, :only => [:add_tag, :remove_tag]
 
   create.before do
     sound.user = current_user
+  end
+
+  def new
+    respond_with(@sound) do |format|
+      format.html do
+        render :layout => "fullscreen"
+      end
+    end
   end
 
   private
