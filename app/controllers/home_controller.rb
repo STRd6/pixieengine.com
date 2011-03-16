498f9fe848fa741b8ea281d6bd7e85cc49c8bf39
Matt Diebolt <mdiebolt@gmail.com>
2011-03-16 00:10:17 -0700

    removed unnecessary action definition

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,10 @@
 class HomeController < ApplicationController
   before_filter :hide_feedback
+  layout "fullscreen"
 
   def sitemap
     @sprite_pages_count = Sprite.count / Sprite.per_page
     @users = User.all(:select => "id, display_name, updated_at")
     @sprites = Sprite.all(:select => "id, title, updated_at", :limit => 10000)
-  end
-
-  def about
-    render :layout => "fullscreen"
   end
 end
