405b040630e7802c0aaa14afc8e48cfda82e80a6
Daniel X Moore <yahivin@gmail.com>
2011-04-07 23:13:39 -0700

    Fixed context menu shittyness

--- public/javascripts/jquery.contextMenu.js
+++ public/javascripts/jquery.contextMenu.js
@@ -23,30 +23,38 @@ if(jQuery)( function() {
 			// 0 needs to be -1 for expected results (no fade)
 			if( o.inSpeed == 0 ) o.inSpeed = -1;
 			if( o.outSpeed == 0 ) o.outSpeed = -1;
+
+      // Get this context menu
+      var menu = $('#' + o.menu);
+
 			// Loop each context menu
 			$(this).each( function() {
 				var el = $(this);
 				var offset = $(el).offset();
-				// Add contextMenu class
-				$('#' + o.menu).addClass('contextMenu');
+
+        var hideMenu = function() {
+          $(document).unbind('click', hideMenu);
+          $(menu).fadeOut(o.outSpeed);
+          return false;
+        };
+
 				// Simulate a true right click
 				$(this).mousedown( function(e) {
-          debugger
 					var evt = e;
 					evt.stopPropagation();
-					$(this).mouseup( function(e) {
-            debugger
+
+          var mouseup = function(e) {
 						e.stopPropagation();
 						var srcElement = $(this);
-						$(this).unbind('mouseup');
+						$(this).unbind('mouseup', mouseup);
+
 						if( evt.button == 2 ) {
 							// Hide context menus that may be showing
-							$(".contextMenu").hide();
-							// Get this context menu
-							var menu = $('#' + o.menu);
-							
+							$(".context_menu").hide();
+
+
 							if( $(el).hasClass('disabled') ) return false;
-							
+
 							// Detect mouse position
 							var d = {}, x, y;
 							if( self.innerHeight ) {
@@ -68,9 +76,8 @@ if(jQuery)( function() {
 							}
 							(e.pageX) ? x = e.pageX : x = e.clientX + d.scrollLeft;
 							(e.pageY) ? y = e.pageY : y = e.clientY + d.scrollTop;
-							
+
 							// Show the menu
-							$(document).unbind('click');
 							$(menu).css({ top: y, left: x }).fadeIn(o.inSpeed);
 							// Hover events
 							$(menu).find('A').mouseover( function() {
@@ -79,55 +86,24 @@ if(jQuery)( function() {
 							}).mouseout( function() {
 								$(menu).find('LI.hover').removeClass('hover');
 							});
-							
-							// Keyboard
-							$(document).keypress( function(e) {
-								switch( e.keyCode ) {
-									case 38: // up
-										if( $(menu).find('LI.hover').size() == 0 ) {
-											$(menu).find('LI:last').addClass('hover');
-										} else {
-											$(menu).find('LI.hover').removeClass('hover').prevAll('LI:not(.disabled)').eq(0).addClass('hover');
-											if( $(menu).find('LI.hover').size() == 0 ) $(menu).find('LI:last').addClass('hover');
-										}
-									break;
-									case 40: // down
-										if( $(menu).find('LI.hover').size() == 0 ) {
-											$(menu).find('LI:first').addClass('hover');
-										} else {
-											$(menu).find('LI.hover').removeClass('hover').nextAll('LI:not(.disabled)').eq(0).addClass('hover');
-											if( $(menu).find('LI.hover').size() == 0 ) $(menu).find('LI:first').addClass('hover');
-										}
-									break;
-									case 13: // enter
-										$(menu).find('LI.hover A').trigger('click');
-									break;
-									case 27: // esc
-										$(document).trigger('click');
-									break
-								}
-							});
-							
+
 							// When items are selected
 							$('#' + o.menu).find('A').unbind('click');
 							$('#' + o.menu).find('LI:not(.disabled) A').click( function() {
-								$(document).unbind('click').unbind('keypress');
-								$(".contextMenu").hide();
+								$(".context_menu").hide();
 								// Callback
 								if( callback ) callback( $(this).attr('href').substr(1), $(srcElement), {x: x - offset.left, y: y - offset.top, docX: x, docY: y} );
 								return false;
 							});
-							
+
 							// Hide bindings
 							setTimeout( function() { // Delay for Mozilla
-								$(document).click( function() {
-									$(document).unbind('click').unbind('keypress');
-									$(menu).fadeOut(o.outSpeed);
-									return false;
-								});
+								$(document).click( hideMenu );
 							}, 0);
 						}
-					});
+					};
+
+					$(this).mouseup( mouseup );
 				});
 				
 				// Disable text selection
@@ -143,16 +119,6 @@ if(jQuery)( function() {
 				
 			});
 			return $(this);
-		},
-		
-		// Destroy context menu(s)
-		destroyContextMenu: function() {
-			// Destroy specified context menus
-			$(this).each( function() {
-				// Disable action
-				$(this).unbind('mousedown').unbind('mouseup');
-			});
-			return( $(this) );
 		}
 		
 	});
