3af98a0515ab072b2f868984c96e5e0cc0cc773e
Daniel X Moore <yahivin@gmail.com>
2011-04-12 10:55:50 -0700

    Improved survey page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,8 @@ class HomeController < ApplicationController
     @users = User.all(:select => "id, display_name, updated_at")
     @sprites = Sprite.all(:select => "id, title, updated_at", :limit => 10000)
   end
+
+  def survey
+    @hide_dock = true
+  end
 end
