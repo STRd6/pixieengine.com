d8d121d9f0b8ba6cbbae6804c8b7abe673710462
Daniel X Moore <yahivin@gmail.com>
2011-05-15 19:35:57 -0700

    Added initial support for nested properties

--- app/coffeescripts/jquery.property_editor.coffee
+++ app/coffeescripts/jquery.property_editor.coffee
@@ -1,28 +1,15 @@
 (($) ->
   $.fn.propertyEditor = (properties) ->
-    element = this.eq(0)
+    object = properties
 
+    element = this.eq(0)
     element.addClass("properties")
 
     element.getProps = () ->
-      props = {}
-
-      element.find("tr:not(.child_property)").each () ->
-        inputs = $(this).find("input")
-
-        if key = inputs.eq(0).val()
-          value = inputs.eq(1).val()
-
-          try
-            props[key] = JSON.parse(value)
-          catch e
-            props[key] = value
-
-          return # This is necessary because the implicit return in the try catch got weird
-
-      props
+      object
 
     element.setProps = (properties) ->
+      object = properties
       element.html('')
 
       if properties
@@ -37,6 +24,8 @@
             addRow(key, value).find('td:last input').colorPicker()
           else if Object.isObject(value) && value.hasOwnProperty('x') && value.hasOwnProperty('y')
             addRow(key, value).find('td:last input').vectorPicker()
+          else if Object.isObject(value)
+            addNestedRow(key, value)
           else
             addRow(key, value)
 
@@ -44,39 +33,90 @@
 
       element
 
+    rowCheck = ->
+      # If last row has data
+      if (input = element.find("tr").last().find("input").first()).length
+        if input.val()
+          addRow('', '')
+      else # Or no rows
+        addRow('', '')
+
+    addBlurEvents = (keyInput, valueInput) ->
+      keyInput.blur ->
+        currentName = keyInput.val()
+        previousName = keyInput.data("previousName")
+
+        return if currentName.blank()
+
+        if currentName != previousName
+          keyInput.data("previousName", currentName)
+          delete object[previousName]
+          object[currentName] = valueInput.val()
+
+          element.trigger("change", object)
+
+          rowCheck()
+
+      valueInput.blur ->
+        currentValue = valueInput.val().parse()
+        previousValue = valueInput.data("previousValue")
+
+        if currentValue != previousValue
+          valueInput.data("previousValue", currentValue)
+          object[keyInput.val()] = currentValue
+
+          element.trigger("change", object)
+
+          rowCheck()
+
     addRow = (key, value) ->
       row = $ "<tr>"
 
       cell = $("<td>").appendTo(row)
 
-      $("<input>",
+      keyInput = $("<input>",
+        class: "key"
+        data:
+          previousName: key
         type: "text"
         placeholder: "key"
         value: key
-      ).appendTo cell
+      ).appendTo(cell)
 
       cell = $("<td>").appendTo(row)
 
       value = JSON.stringify(value) unless typeof value == "string"
 
-      $("<input>",
+      valueInput = $("<input>",
+        class: "value"
+        data:
+          previousValue: value
         type: "text"
         placeholder: "value"
         value: value
-      ).appendTo cell
+      ).appendTo(cell)
 
-      row.appendTo element
+      addBlurEvents(keyInput, valueInput)
 
-    $('input', this.selector).live 'blur', (event) ->
-      element.trigger("change", element.getProps())
+      return row.appendTo(element)
 
-      $this = $(this)
+    addNestedRow = (key, value) ->
+      row = $("<tr>")
+      cell = $("<td colspan='2'>").appendTo(row)
 
-      if (input = element.find("tr").last().find("input").first()).length
-        if input.val()
-          addRow('', '')
-      else
-        addRow('', '')
+      #TODO: Editable key
+
+      $("<label>",
+        text: key
+      ).appendTo(cell)
+
+      nestedEditor = $("<table>",
+        class: "nested"
+      ).appendTo(cell).propertyEditor(value)
+
+      #TODO Cascade change events
+
+      return row.appendTo(element)
 
     element.setProps(properties)
 
--- public/javascripts/jquery.property_editor.js
+++ public/javascripts/jquery.property_editor.js
@@ -1,33 +1,20 @@
-/* DO NOT MODIFY. This file was compiled Sun, 15 May 2011 21:33:21 GMT from
+/* DO NOT MODIFY. This file was compiled Mon, 16 May 2011 02:33:40 GMT from
  * /home/daniel/apps/pixie.strd6.com/app/coffeescripts/jquery.property_editor.coffee
  */
 
 (function() {
   (function($) {
     return $.fn.propertyEditor = function(properties) {
-      var addRow, element;
+      var addBlurEvents, addNestedRow, addRow, element, object, rowCheck;
+      object = properties;
       element = this.eq(0);
       element.addClass("properties");
       element.getProps = function() {
-        var props;
-        props = {};
-        element.find("tr:not(.child_property)").each(function() {
-          var inputs, key, value;
-          inputs = $(this).find("input");
-          if (key = inputs.eq(0).val()) {
-            value = inputs.eq(1).val();
-            try {
-              props[key] = JSON.parse(value);
-            } catch (e) {
-              props[key] = value;
-            }
-            return;
-          }
-        });
-        return props;
+        return object;
       };
       element.setProps = function(properties) {
         var key, propertiesArray, value;
+        object = properties;
         element.html('');
         if (properties) {
           propertiesArray = [];
@@ -41,6 +28,8 @@
               return addRow(key, value).find('td:last input').colorPicker();
             } else if (Object.isObject(value) && value.hasOwnProperty('x') && value.hasOwnProperty('y')) {
               return addRow(key, value).find('td:last input').vectorPicker();
+            } else if (Object.isObject(value)) {
+              return addNestedRow(key, value);
             } else {
               return addRow(key, value);
             }
@@ -49,11 +38,53 @@
         addRow('', '');
         return element;
       };
+      rowCheck = function() {
+        var input;
+        if ((input = element.find("tr").last().find("input").first()).length) {
+          if (input.val()) {
+            return addRow('', '');
+          }
+        } else {
+          return addRow('', '');
+        }
+      };
+      addBlurEvents = function(keyInput, valueInput) {
+        keyInput.blur(function() {
+          var currentName, previousName;
+          currentName = keyInput.val();
+          previousName = keyInput.data("previousName");
+          if (currentName.blank()) {
+            return;
+          }
+          if (currentName !== previousName) {
+            keyInput.data("previousName", currentName);
+            delete object[previousName];
+            object[currentName] = valueInput.val();
+            element.trigger("change", object);
+            return rowCheck();
+          }
+        });
+        return valueInput.blur(function() {
+          var currentValue, previousValue;
+          currentValue = valueInput.val().parse();
+          previousValue = valueInput.data("previousValue");
+          if (currentValue !== previousValue) {
+            valueInput.data("previousValue", currentValue);
+            object[keyInput.val()] = currentValue;
+            element.trigger("change", object);
+            return rowCheck();
+          }
+        });
+      };
       addRow = function(key, value) {
-        var cell, row;
+        var cell, keyInput, row, valueInput;
         row = $("<tr>");
         cell = $("<td>").appendTo(row);
-        $("<input>", {
+        keyInput = $("<input>", {
+          "class": "key",
+          data: {
+            previousName: key
+          },
           type: "text",
           placeholder: "key",
           value: key
@@ -62,25 +93,30 @@
         if (typeof value !== "string") {
           value = JSON.stringify(value);
         }
-        $("<input>", {
+        valueInput = $("<input>", {
+          "class": "value",
+          data: {
+            previousValue: value
+          },
           type: "text",
           placeholder: "value",
           value: value
         }).appendTo(cell);
+        addBlurEvents(keyInput, valueInput);
         return row.appendTo(element);
       };
-      $('input', this.selector).live('blur', function(event) {
-        var $this, input;
-        element.trigger("change", element.getProps());
-        $this = $(this);
-        if ((input = element.find("tr").last().find("input").first()).length) {
-          if (input.val()) {
-            return addRow('', '');
-          }
-        } else {
-          return addRow('', '');
-        }
-      });
+      addNestedRow = function(key, value) {
+        var cell, nestedEditor, row;
+        row = $("<tr>");
+        cell = $("<td colspan='2'>").appendTo(row);
+        $("<label>", {
+          text: key
+        }).appendTo(cell);
+        nestedEditor = $("<table>", {
+          "class": "nested"
+        }).appendTo(cell).propertyEditor(value);
+        return row.appendTo(element);
+      };
       return element.setProps(properties);
     };
   })(jQuery);
