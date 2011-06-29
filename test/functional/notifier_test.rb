4cccdd510e34eef2eea4a81464dc1483ed70935e
Matt Diebolt <mdiebolt@gmail.com>
2011-06-28 22:59:33 -0700

    adding test for analytics Notifier method

--- test/functional/notifier_test.rb
+++ test/functional/notifier_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class NotifierTest < ActionMailer::TestCase
   setup do
     @feedback = Feedback.create(:email_address => "testy@testy.com", :body => "Help I'm A test!")
+    @user = User.create(:email => "test@strd6.com", :display_name => "Testman")
   end
 
   test "received_feedback" do
@@ -10,6 +11,12 @@ class NotifierTest < ActionMailer::TestCase
     assert_equal "Received feedback", mail.subject
     assert_equal ["notifications@strd6.com"], mail.from
     assert_equal [@feedback.email_address], mail.reply_to
+  end
+
+  test "analytics" do
+    mail = Notifier.analytics(@user)
+    assert_equal "Weekly Analytics", mail.subject
+    assert_equal ["notifications@strd6.com"], mail.from
   end
 
 end
