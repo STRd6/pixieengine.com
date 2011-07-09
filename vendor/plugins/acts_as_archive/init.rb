7c71bdca7bb282adc84754625e2d0073eed63e7e
Matt Diebolt <mdiebolt@gmail.com>
2011-07-09 00:12:30 -0700

    moving init file to stay on top of deprecation warnings

--- vendor/plugins/acts_as_archive/init.rb
+++ vendor/plugins/acts_as_archive/init.rb
@@ -1,1 +1,6 @@
-require File.dirname(__FILE__) + "/rails/init"
+require File.expand_path("#{File.dirname(__FILE__)}/require")
+
+require "active_record"
+
+ActiveRecord::Base.extend(ActsAsArchive::Base::ActMethods)
+ActiveRecord::Migration.send(:include, ActsAsArchive::Migration)
