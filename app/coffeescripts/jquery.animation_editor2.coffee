6035a50e37d382c09a01446bef825692a37d74cb
Matt Diebolt <mdiebolt@gmail.com>
2011-07-28 16:53:15 -0700

    adding first basic functionality to animation editor

--- app/coffeescripts/jquery.animation_editor2.coffee
+++ app/coffeescripts/jquery.animation_editor2.coffee
@@ -1,16 +1,78 @@
 $.fn.animationEditor = ->
   animationNumber = 1
+  animations = []
 
-  animation:
-    name: "Animation #{animationNumber}"
-    tileset: []
-    sequences: []
+  Controls = ->
+    playing = false
+    paused = false
 
-  addAnimation: ->
+    scrubberEl = $('.scrubber')
+
+    scrubber =
+      min: scrubberEl.get(0).min
+      max: scrubberEl.get(0).max
+      val: scrubberEl.val()
+
+    fpsEl = $('.fps input')
+
+    fps =
+      min: fpsEl.get(0).min
+      max: fpsEl.get(0).max
+      val: fpsEl.val()
+
+    self =
+      play: ->
+        scrubber.val = (scrubber.val + 1) % scrubber.max
+
+      stop: ->
+        scrubber.val = 0
+
+      update: ->
+        scrubberEl.val(scrubber.val)
+
+    return self
+
+  Animation = (name) ->
+    tileset = []
+    sequences = []
+    currentFrameIndex = 0
+
+    name ||= "Animation #{animationNumber}"
     animationNumber += 1
+
+    self =
+      addFrame: (image) ->
+        tileset.push(image)
+
+      advanceFrame: ->
+        currentFrameIndex = (currentFrameIndex + 1) % tileset.length
+
+      name: name
+
+      removeFrame: (image) ->
+        tileset.remove(image)
+
+    return self
+
+  animations.push(Animation())
 
   animationEditor = $(this.get(0)).addClass("editor animation_editor")
 
   templates = $("#animation_editor_templates")
+  animationTemplate = templates.find('.animation')
 
   templates.find(".editor.template").tmpl().appendTo(animationEditor)
+
+  controls = Controls()
+
+  updateUI = ->
+    for animation in animations
+      animations = $('.animations')
+
+      animations.children().remove()
+      animationTemplate.tmpl(animation.name).appendTo(animations)
+
+  updateUI()
+
+  controls.play()
+  controls.update()
--- public/javascripts/jquery.animation_editor2.js
+++ public/javascripts/jquery.animation_editor2.js
@@ -1,23 +1,81 @@
-/* DO NOT MODIFY. This file was compiled Tue, 26 Jul 2011 19:18:07 GMT from
+/* DO NOT MODIFY. This file was compiled Thu, 28 Jul 2011 23:52:37 GMT from
  * /Users/matt/pixie.strd6.com/app/coffeescripts/jquery.animation_editor2.coffee
  */
 
 (function() {
   $.fn.animationEditor = function() {
-    var animationEditor, animationNumber, templates;
+    var Animation, Controls, animationEditor, animationNumber, animationTemplate, animations, controls, templates, updateUI;
     animationNumber = 1;
-    ({
-      animation: {
-        name: "Animation " + animationNumber,
-        tileset: [],
-        sequences: []
-      },
-      addAnimation: function() {
-        return animationNumber += 1;
-      }
-    });
+    animations = [];
+    Controls = function() {
+      var fps, fpsEl, paused, playing, scrubber, scrubberEl, self;
+      playing = false;
+      paused = false;
+      scrubberEl = $('.scrubber');
+      scrubber = {
+        min: scrubberEl.get(0).min,
+        max: scrubberEl.get(0).max,
+        val: scrubberEl.val()
+      };
+      fpsEl = $('.fps input');
+      fps = {
+        min: fpsEl.get(0).min,
+        max: fpsEl.get(0).max,
+        val: fpsEl.val()
+      };
+      self = {
+        play: function() {
+          return scrubber.val = (scrubber.val + 1) % scrubber.max;
+        },
+        stop: function() {
+          return scrubber.val = 0;
+        },
+        update: function() {
+          return scrubberEl.val(scrubber.val);
+        }
+      };
+      return self;
+    };
+    Animation = function(name) {
+      var currentFrameIndex, self, sequences, tileset;
+      tileset = [];
+      sequences = [];
+      currentFrameIndex = 0;
+      name || (name = "Animation " + animationNumber);
+      animationNumber += 1;
+      self = {
+        addFrame: function(image) {
+          return tileset.push(image);
+        },
+        advanceFrame: function() {
+          return currentFrameIndex = (currentFrameIndex + 1) % tileset.length;
+        },
+        name: name,
+        removeFrame: function(image) {
+          return tileset.remove(image);
+        }
+      };
+      return self;
+    };
+    animations.push(Animation());
     animationEditor = $(this.get(0)).addClass("editor animation_editor");
     templates = $("#animation_editor_templates");
-    return templates.find(".editor.template").tmpl().appendTo(animationEditor);
+    animationTemplate = templates.find('.animation');
+    templates.find(".editor.template").tmpl().appendTo(animationEditor);
+    controls = Controls();
+    updateUI = function() {
+      var animation, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = animations.length; _i < _len; _i++) {
+        animation = animations[_i];
+        animations = $('.animations');
+        animations.children().remove();
+        _results.push(animationTemplate.tmpl(animation.name).appendTo(animations));
+      }
+      return _results;
+    };
+    updateUI();
+    controls.play();
+    return controls.update();
   };
 }).call(this);
