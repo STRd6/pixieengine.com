7e35a3758e486dc1a46f1a1c290b280d891cf142
Matt Diebolt <mdiebolt@gmail.com>
2011-07-29 15:05:10 -0700

    click to add sprite to animation frame

--- app/coffeescripts/jquery.animation_editor2.coffee
+++ app/coffeescripts/jquery.animation_editor2.coffee
@@ -1,9 +1,7 @@
-$.fn.animationEditor = ->
+$.fn.animationEditor = (options) ->
   animationNumber = 1
 
   Controls = ->
-    paused = false
-
     intervalId = null
 
     scrubberEl = $('.scrubber')
@@ -24,6 +22,16 @@ $.fn.animationEditor = ->
       scrubber.val = (scrubber.val + 1) % scrubber.max
       self.update()
 
+    changePlayIcon = (icon) ->
+      el = $('.play')
+
+      el.css "background-image", "url(/images/#{icon}.png)"
+
+      if icon == 'pause'
+        el.addClass('pause')
+      else
+        el.removeClass('pause')
+
     self =
       fps: (val) ->
         if val?
@@ -33,14 +41,26 @@ $.fn.animationEditor = ->
         else
           return fps.val
 
+      pause: ->
+        changePlayIcon('play')
+        clearInterval(intervalId)
+        intervalId = null
+
       play: ->
         intervalId = setInterval(updateFrame, 1000 / fps.val) unless intervalId
+        changePlayIcon('pause')
+        stopped = false
+
+      scrubberPosition: ->
+        "#{scrubber.val} / #{scrubber.max}"
 
       stop: ->
         scrubber.val = 0
         self.update()
         clearInterval(intervalId)
         intervalId = null
+        changePlayIcon('play')
+        stopped = true
 
       update: ->
         scrubberEl.val(scrubber.val)
@@ -58,6 +78,10 @@ $.fn.animationEditor = ->
     animationNumber += 1
 
     self =
+      addFrame: (imgSrc) ->
+        tileset.push(imgSrc) if $.inArray(imgSrc, tileset) == -1
+        frames.push(tileset.indexOf(imgSrc))
+
       name: name
 
     return self
@@ -67,24 +91,61 @@ $.fn.animationEditor = ->
   templates = $("#animation_editor_templates")
   editorTemplate = templates.find('.editor.template')
   animationTemplate = templates.find('.animation')
+  spriteTemplate = templates.find('.sprite')
 
   editorTemplate.tmpl().appendTo(animationEditor)
 
   controls = Controls()
 
-  animations = [Animation()]
+  animation = Animation()
+
+  animations = [animation]
 
   updateUI = ->
+    sprites = $('.sprites')
+
     for animation in animations
       animations = $('.animations')
 
       animations.children().remove()
       animationTemplate.tmpl(animation.name).appendTo(animations)
 
+    spritesSrc = [
+      "http://dev.pixie.strd6.com/sprites/15408/original.png"
+      "http://dev.pixie.strd6.com/sprites/15407/original.png"
+      "http://dev.pixie.strd6.com/sprites/15406/original.png"
+      "http://dev.pixie.strd6.com/sprites/15405/original.png"
+      "http://dev.pixie.strd6.com/sprites/15404/original.png"
+      "http://dev.pixie.strd6.com/sprites/15403/original.png"
+      "http://dev.pixie.strd6.com/sprites/15402/original.png"
+      "http://dev.pixie.strd6.com/sprites/15401/original.png"
+      "http://dev.pixie.strd6.com/sprites/15400/original.png"
+      "http://dev.pixie.strd6.com/sprites/15399/original.png"
+      "http://dev.pixie.strd6.com/sprites/15398/original.png"
+      "http://dev.pixie.strd6.com/sprites/15397/original.png"
+      "http://dev.pixie.strd6.com/sprites/15396/original.png"
+      "http://dev.pixie.strd6.com/sprites/15395/original.png"
+      "http://dev.pixie.strd6.com/sprites/15394/original.png"
+      "http://dev.pixie.strd6.com/sprites/15393/original.png"
+    ]
+
+    for src in spritesSrc
+      spriteTemplate.tmpl(src: src).appendTo(sprites)
+
   updateUI()
 
-  $('.play').mousedown -> controls.play()
+  $('.play').mousedown ->
+    if $(this).hasClass('pause')
+      controls.pause()
+    else
+      controls.play()
+
   $('.stop').mousedown -> controls.stop()
+
+  $('.sprites .sprite_container').mousedown ->
+    imgSrc = $(this).find('img').attr('src')
+
+    animation.addFrame(imgSrc)
 
   $('.fps input').change ->
     newValue = $(this).val()
--- public/javascripts/jquery.animation_editor2.js
+++ public/javascripts/jquery.animation_editor2.js
@@ -1,14 +1,13 @@
-/* DO NOT MODIFY. This file was compiled Fri, 29 Jul 2011 00:50:21 GMT from
+/* DO NOT MODIFY. This file was compiled Fri, 29 Jul 2011 22:04:52 GMT from
  * /Users/matt/pixie.strd6.com/app/coffeescripts/jquery.animation_editor2.coffee
  */
 
 (function() {
-  $.fn.animationEditor = function() {
-    var Animation, Controls, animationEditor, animationNumber, animationTemplate, animations, controls, editorTemplate, templates, updateUI;
+  $.fn.animationEditor = function(options) {
+    var Animation, Controls, animation, animationEditor, animationNumber, animationTemplate, animations, controls, editorTemplate, spriteTemplate, templates, updateUI;
     animationNumber = 1;
     Controls = function() {
-      var fps, fpsEl, intervalId, paused, scrubber, scrubberEl, self, updateFrame;
-      paused = false;
+      var changePlayIcon, fps, fpsEl, intervalId, scrubber, scrubberEl, self, updateFrame;
       intervalId = null;
       scrubberEl = $('.scrubber');
       scrubber = {
@@ -26,6 +25,16 @@
         scrubber.val = (scrubber.val + 1) % scrubber.max;
         return self.update();
       };
+      changePlayIcon = function(icon) {
+        var el;
+        el = $('.play');
+        el.css("background-image", "url(/images/" + icon + ".png)");
+        if (icon === 'pause') {
+          return el.addClass('pause');
+        } else {
+          return el.removeClass('pause');
+        }
+      };
       self = {
         fps: function(val) {
           if (val != null) {
@@ -36,16 +45,30 @@
             return fps.val;
           }
         },
+        pause: function() {
+          changePlayIcon('play');
+          clearInterval(intervalId);
+          return intervalId = null;
+        },
         play: function() {
+          var stopped;
           if (!intervalId) {
-            return intervalId = setInterval(updateFrame, 1000 / fps.val);
+            intervalId = setInterval(updateFrame, 1000 / fps.val);
           }
+          changePlayIcon('pause');
+          return stopped = false;
+        },
+        scrubberPosition: function() {
+          return "" + scrubber.val + " / " + scrubber.max;
         },
         stop: function() {
+          var stopped;
           scrubber.val = 0;
           self.update();
           clearInterval(intervalId);
-          return intervalId = null;
+          intervalId = null;
+          changePlayIcon('play');
+          return stopped = true;
         },
         update: function() {
           scrubberEl.val(scrubber.val);
@@ -63,6 +86,12 @@
       name || (name = "Animation " + animationNumber);
       animationNumber += 1;
       self = {
+        addFrame: function(imgSrc) {
+          if ($.inArray(imgSrc, tileset) === -1) {
+            tileset.push(imgSrc);
+          }
+          return frames.push(tileset.indexOf(imgSrc));
+        },
         name: name
       };
       return self;
@@ -71,26 +100,45 @@
     templates = $("#animation_editor_templates");
     editorTemplate = templates.find('.editor.template');
     animationTemplate = templates.find('.animation');
+    spriteTemplate = templates.find('.sprite');
     editorTemplate.tmpl().appendTo(animationEditor);
     controls = Controls();
-    animations = [Animation()];
+    animation = Animation();
+    animations = [animation];
     updateUI = function() {
-      var animation, _i, _len, _results;
-      _results = [];
+      var animation, sprites, spritesSrc, src, _i, _j, _len, _len2, _results;
+      sprites = $('.sprites');
       for (_i = 0, _len = animations.length; _i < _len; _i++) {
         animation = animations[_i];
         animations = $('.animations');
         animations.children().remove();
-        _results.push(animationTemplate.tmpl(animation.name).appendTo(animations));
+        animationTemplate.tmpl(animation.name).appendTo(animations);
+      }
+      spritesSrc = ["http://dev.pixie.strd6.com/sprites/15408/original.png", "http://dev.pixie.strd6.com/sprites/15407/original.png", "http://dev.pixie.strd6.com/sprites/15406/original.png", "http://dev.pixie.strd6.com/sprites/15405/original.png", "http://dev.pixie.strd6.com/sprites/15404/original.png", "http://dev.pixie.strd6.com/sprites/15403/original.png", "http://dev.pixie.strd6.com/sprites/15402/original.png", "http://dev.pixie.strd6.com/sprites/15401/original.png", "http://dev.pixie.strd6.com/sprites/15400/original.png", "http://dev.pixie.strd6.com/sprites/15399/original.png", "http://dev.pixie.strd6.com/sprites/15398/original.png", "http://dev.pixie.strd6.com/sprites/15397/original.png", "http://dev.pixie.strd6.com/sprites/15396/original.png", "http://dev.pixie.strd6.com/sprites/15395/original.png", "http://dev.pixie.strd6.com/sprites/15394/original.png", "http://dev.pixie.strd6.com/sprites/15393/original.png"];
+      _results = [];
+      for (_j = 0, _len2 = spritesSrc.length; _j < _len2; _j++) {
+        src = spritesSrc[_j];
+        _results.push(spriteTemplate.tmpl({
+          src: src
+        }).appendTo(sprites));
       }
       return _results;
     };
     updateUI();
     $('.play').mousedown(function() {
-      return controls.play();
+      if ($(this).hasClass('pause')) {
+        return controls.pause();
+      } else {
+        return controls.play();
+      }
     });
     $('.stop').mousedown(function() {
       return controls.stop();
+    });
+    $('.sprites .sprite_container').mousedown(function() {
+      var imgSrc;
+      imgSrc = $(this).find('img').attr('src');
+      return animation.addFrame(imgSrc);
     });
     return $('.fps input').change(function() {
       var newValue;
