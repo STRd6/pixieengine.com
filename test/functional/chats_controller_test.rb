6b19e54fc85f08548cf1d935b2049adee956b04e
Daniel X. Moore <yahivin@gmail.com>
2011-08-09 21:39:37 -0700

    Added test for chats eating images and fixed for Rails 3.1

--- test/functional/chats_controller_test.rb
+++ test/functional/chats_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ChatsControllerTest < ActionController::TestCase
   setup do
     @user = log_in
+    @image_text = "<img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAqElEQVRYR+2WwQ6AMAhDt///aM0OJMsCFMiUS73q2kcBdY7mazb7DwIwgVsJPGCYTR8EgIR3X0tLNNT7HoB7MLm+S6sEgBKKcqQBrAN7SzS4s2XyTAlgVXea/AawzDXqdoBIz7XhTbfASqAC8MkamhUphGUALwU0C8IB3yVoz6GA05PQWQQgKWgr6c1DyDwjmo08rB1JYK8UfZyyevwlYwJMgAkwgf4EXnGqJiHjLmjqAAAAAElFTkSuQmCC\">"
   end
 
   should "create a new Chat" do
@@ -11,21 +12,9 @@ class ChatsControllerTest < ActionController::TestCase
     end
   end
 
-  # should "get recent" do
-  #   get :recent
-  #
-  #   assert_response :not_modified
-  # end
-  #
-  # should "get active_users" do
-  #   get :active_users
-  #
-  #   assert_response :not_modified
-  # end
+  should "preserve images" do
+    post :create, :body => @image_text
 
-  should "have the correct routes" do
-    assert_routing({ :path => "chats", :method => :post }, { :controller => "chats", :action => "create" })
-    assert_routing({ :path => "chats/recent", :method => :get }, { :controller => "chats", :action => "recent" })
-    assert_routing({ :path => "chats/active_users", :method => :get }, { :controller => "chats", :action => "active_users" })
+    assert Chat.last.text.length == @image_text.length
   end
 end
