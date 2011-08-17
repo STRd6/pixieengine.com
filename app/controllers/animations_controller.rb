3ee3c867bb5f7e0f8701e5ed3ff10467f18acc65
Matt Diebolt <mdiebolt@gmail.com>
2011-08-17 14:16:21 -0700

    removing unused controller actions

--- app/controllers/animations_controller.rb
+++ app/controllers/animations_controller.rb
@@ -1,25 +1,7 @@
 class AnimationsController < ApplicationController
   respond_to :html, :json
 
-  before_filter :require_user, :except => [:index]
-
   def new
-  end
-
-  def create
-    @animation = Animation.new(params[:animation])
-    @animation.user = current_user
-
-    @animation.save
-
-    respond_with(@animation)
-  end
-
-  def edit
-    @animation = Animation.find(params[:id])
-    @user_sprites = current_user.sprites
-
-    respond_with(@animation)
   end
 
   def download
