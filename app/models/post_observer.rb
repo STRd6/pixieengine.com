e429544e6bcbd5fa743f4fa3790b6b0359adb0e7
Matt Diebolt <mdiebolt@gmail.com>
2011-08-22 17:06:29 -0700

    adding daniel are I as recipients of all forum emails

--- app/models/post_observer.rb
+++ app/models/post_observer.rb
@@ -2,7 +2,10 @@ class PostObserver < ActiveRecord::Observer
   observe Forem::Post
 
   def after_save(post)
-    users = post.topic.posts.map(&:user).uniq - [post.user]
+    daniel = User.find 1
+    matt = User.find 4
+
+    users = (post.topic.posts.map(&:user).uniq - [post.user] + [daniel, matt]).uniq
 
     users.each do |user|
       Notifier.new_post(post, user).deliver if user.forum_notifications
