35189aac87e7ff0f86fc1993497fe3be07fb1b4b
Matt Diebolt <mdiebolt@gmail.com>
2011-10-09 14:16:21 -0700

    checking for a blank avatar in user_link notifier helper

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -1,11 +1,13 @@
 module NotifierHelper
   def project_preview_left(project)
     return unless project.image.exists?
+
     link_to image_tag(project.image.url(:thumb), :alt => project.display_name, :class => "left", :style => "display:inline-block;float:left;margin-right:1em;width:96px;height:96px;"), fullscreen_project_url(project, @link_tracking)
   end
 
   def project_preview_right(project)
     return unless project.image.exists?
+
     link_to image_tag(project.image.url(:thumb), :alt => project.display_name, :class => "right", :style => "display:inline-block;float:right;margin-left:1em;width:96px;height:96px;"), fullscreen_project_url(project, @link_tracking)
   end
 
@@ -26,7 +28,9 @@ module NotifierHelper
   end
 
   def user_link(user, options=nil)
-    if options && options[:text]
+    if user.avatar.exists? == false
+      "#{link_to(user, user_url(user, @link_tracking))}".html_safe
+    elsif (options && options[:text])
       user_url(user, @link_tracking)
     else
       "#{link_to image_tag(user.avatar.url(:thumb), :alt => user.display_name, :class => :avatar), user_url(user, @link_tracking)} #{link_to user, user_url(user, @link_tracking)}".html_safe
