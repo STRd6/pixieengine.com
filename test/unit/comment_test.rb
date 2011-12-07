f9277d5b822eb372306e78d934e668e58564b997
Matt Diebolt <mdiebolt@gmail.com>
2011-12-06 20:38:38 -0800

    adding test to prevent regression for as_json error due to nil commentable

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -6,4 +6,13 @@ class CommentTest < ActiveSupport::TestCase
       Factory :comment
     end
   end
+
+  should "have working as_json method even if the commentable has been deleted" do
+    comment = Factory :comment
+    comment.stubs(:commentable).returns(nil)
+
+    assert_nothing_raised do
+      comment.as_json
+    end
+  end
 end
