e612407b7edcf675b57ea76feba7911bf2b0998d
Daniel X. Moore <yahivin@gmail.com>
2011-12-15 22:30:22 -0800

    bulbin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -228,6 +228,8 @@ class ProjectsController < ApplicationController
   end
 
   def ide
+    @show_bulb = true
+
     respond_with(project) do |format|
       format.html do
         @has_reg_popup = true
