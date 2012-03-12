82fdbcbc41fc0e28cde1c7b970ab498b7902a6e0
Matt Diebolt <mdiebolt@gmail.com>
2012-03-11 22:51:07 -0700

    removing post_test now that forem is gone

--- test/unit/post_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MembershipTest < ActiveSupport::TestCase
-  should "queue email with Delayed Job on create" do
-    assert_difference 'Delayed::Job.count', +1 do
-      Factory :post
-    end
-  end
-end
