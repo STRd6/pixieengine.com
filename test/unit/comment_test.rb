3c0d635533055a4ec019faaad4257ae8203963af
Daniel X. Moore <yahivin@gmail.com>
2012-04-05 22:27:09 -0700

    Removed tests for delayed job items.

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  should "queue email with Delayed Job on create" do
-    assert_difference 'Delayed::Job.count', +1 do
-      Factory :comment
-    end
-  end
-
   should "have working as_json method even if the commentable has been deleted" do
     comment = Factory :comment
     comment.stubs(:commentable).returns(nil)
--- test/unit/membership_test.rb
+++ test/unit/membership_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class MembershipTest < ActiveSupport::TestCase
-  should "queue email with Delayed Job on create" do
-    assert_difference 'Delayed::Job.count', +1 do
-      Factory :membership
-    end
-  end
 end
