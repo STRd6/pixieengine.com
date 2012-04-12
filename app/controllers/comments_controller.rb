9bb5c854caf8e2decf4e6d0c5527aaec79b01b33
Daniel X. Moore <yahivin@gmail.com>
2012-04-12 14:37:02 -0700

    Fixed route path names

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   respond_to :html, :json
-  before_filter :require_user
+  before_filter :require_user, :except => :index
 
   def create
     comment_params = params[:comment]
