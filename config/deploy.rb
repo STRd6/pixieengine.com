f29c3ee94fc5721e018c2bd8ccc782373e5daa7a
Daniel Moore <yahivin@gmail.com>
2012-12-28 19:27:10 -0800

    Added migrate back to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ after :deploy do
   run "chmod -R g+w #{release_path}/tmp"
   run "chmod -R g+w #{release_path}/.bundle"
 end
-# after :deploy, "deploy:migrate"
+after :deploy, "deploy:migrate"
 after :deploy, "deploy:cleanup"
 
 # Whenever task
