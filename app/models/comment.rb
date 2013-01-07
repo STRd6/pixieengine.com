fb1feab6ac83a434e1edfbfbd4c631d31c2c9c89
Daniel Moore <yahivin@gmail.com>
2013-01-06 23:38:07 -0800

    Comment data and light styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ActiveRecord::Base
   include Sanitization
+  include Rails.application.routes.url_helpers
+
+  self.default_url_options = { :host => nil }
 
   belongs_to :commenter, :class_name => "User"
   belongs_to :commentee, :class_name => "User"
@@ -30,17 +33,41 @@ class Comment < ActiveRecord::Base
   scope :for_user, lambda {|user| where(:commentee_id => user)}
 
   def as_json(options={})
-    {
+    data = {
       :commenter_id => commenter_id,
       :commenter_name => commenter.display_name,
       :id => id,
-      :commentable_id => commentable ? commentable.id : -1,
-      :commentable_type => commentable_type.downcase.pluralize,
-      :commentable_img_src => commentable ? commentable.image.url(:thumb) : "",
       :avatar_src => commenter.avatar.url(:thumb),
       :body => sanitize(BlueCloth.new(body).to_html),
       :time => created_at.getutc.iso8601
     }
+
+    if commentable
+      type = commentable_type.downcase
+
+      data[:commentable] = {
+        id: commentable.id,
+        name: commentable.display_name,
+        url: send("#{type}_path", commentable),
+        type: type
+      }
+
+      if commentable.is_a? Sprite
+        data[:image] = {
+          :src => commentable.image.url,
+          :width => commentable.width,
+          :height => commentable.height
+        }
+      else
+        data[:image] = {
+          :src => commentable.image.url(:thumb),
+          :width => 96,
+          :height => 96
+        }
+      end
+    end
+
+    return data
   end
 
   def notify_commentee
