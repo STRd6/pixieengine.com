d0fbca3199048688c700e84c286dc81a1f23eea8
Daniel Moore <yahivin@gmail.com>
2013-01-12 19:04:46 -0800

    Fixed assoc

--- lib/bogus_mail_interceptor.rb
+++ lib/bogus_mail_interceptor.rb
@@ -2,7 +2,7 @@ class BogusMailInterceptor
   def self.delivering_email(message)
     email = message.to
 
-    if Email.where{:email => email, :undeliverable => true}.any?
+    if Email.where(:email => email, :undeliverable => true).any?
       message.perform_deliveries = false
     end
   end
