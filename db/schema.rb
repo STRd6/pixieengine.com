d95d5fcffb49c09f75fdb34ef9cfe9f50406429a
Daniel X Moore <yahivin@gmail.com>
2014-06-19 11:32:51 -0700

    Displaying activity on user page and backfilling comment activity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140618153703) do
+ActiveRecord::Schema.define(:version => 20140619180815) do
 
   create_table "access_tokens", :force => true do |t|
     t.integer  "user_id"
