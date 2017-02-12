314c05e54956b823d61099a1a1bf9e819b219d14
Daniel X. Moore <yahivin@gmail.com>
2017-02-11 21:46:24 -0800

    Removed mail initializer

--- config/initializers/mail.rb
+++
@@ -1,7 +0,0 @@
-Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.staging?
-
-if Rails.env.production? or Rails.env.staging?
-  ActionMailer::Base.smtp_settings = ActionMailer::Base.smtp_settings.merge({
-    :password => Rails.application.secrets.mailgun_password
-  })
-end
