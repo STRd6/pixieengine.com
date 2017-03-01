fc457665e730c000fe9ae39b4ad155750ded422e
Daniel X. Moore <yahivin@gmail.com>
2017-02-28 18:51:01 -0800

    Fixed tunes publishing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,11 @@
 //= require ajax
 //= require lib/isotope.min
 
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+  }
+});
+
 function trackPageview(e){return _gaq.push(["_trackPageview",e])}
 function trackEvent(e,t,n){return _gaq.push(["_trackEvent",e,t,n])}
